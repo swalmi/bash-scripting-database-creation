e8ef3976d1501a2fac1bfaf579da5a2a566a4ef6
unknown <abdullahalswalmi@gmail.com>
2026-02-09 22:19:13 +0200

    feat: select-table-updated

--- BashDBMS/lib/select_row.sh
+++ BashDBMS/lib/select_row.sh
@@ -4,12 +4,12 @@ source ./lib/validation.sh
 DB_PATH="./databases/$db_name"
 
 echo "--- Select from Table in $db_name ---"
-# List tables using your standard logic
 ls "$DB_PATH"/*.table 2>/dev/null | xargs -n 1 basename | sed 's/\.table//'
 echo "------------------------------------"
 
 read -p "Enter Table Name: " table_name
 table_file="$DB_PATH/${table_name}.data"
+table_schema="$DB_PATH/${table_name}.table"
 
 if [ ! -f "$table_file" ]; then
     echo "Error: Table '$table_name' does not exist."
@@ -22,14 +22,29 @@ read -p "Choose option: " choice
 
 case $choice in
     1)
-        cat "$table_file"
+        echo ""
+        echo "----------------------------------------------"
+        # Print ALL column names (not just first line)
+        cat "$table_schema" | cut -d: -f1 | tr '\n' '\t'
+        echo ""
+        echo "----------------------------------------------"
+        # Print data
+        cat "$table_file" | tr ':' '\t'
+        echo ""
+        echo "----------------------------------------------"
         ;;
     2)
         read -p "Enter ID to search: " pk
-        # Grep exactly the start of the line for the PK
         result=$(grep -w "^$pk" "$table_file")
         if [ -n "$result" ]; then
-            echo "Result: $result"
+            echo ""
+            echo "----------------------------------------------"
+            cat "$table_schema" | cut -d: -f1 | tr '\n' '\t'
+            echo ""
+            echo "----------------------------------------------"
+            echo "$result" | tr ':' '\t'
+            echo ""
+            echo "----------------------------------------------"
         else
             echo "No record found with ID $pk."
         fi
